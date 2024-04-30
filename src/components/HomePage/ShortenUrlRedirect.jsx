619c5c8d0163491f16db4b0a3350677a1a1ad0c5
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-04-30 17:21:30 +0530

    added ad network

--- src/components/HomePage/ShortenUrlRedirect.jsx
+++ src/components/HomePage/ShortenUrlRedirect.jsx
@@ -4,13 +4,15 @@ import { apiConnector } from '../../services/apiConnector';
 import { guestEndPoints } from '../../services/apis';
 import { toast } from "react-hot-toast"
 import Spinner from '../common/Spinner';
+import AdComponent from '../common/AdComponent';
 
-const { GET_TEMP_SHORT_URL_API, } = guestEndPoints
+const { GET_TEMP_SHORT_URL_API, } = guestEndPoints;
 
 export default function ShortUrlRedirect() {
   const { shortUrl } = useParams();
   const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState('');
+  const [iframeCompatible, setIframeCompatible] = useState(true);
 
   const redirect = async () => {
     setLoading(true);
@@ -20,7 +22,7 @@ export default function ShortUrlRedirect() {
         setUrl(response.data.baseUrl);
       }
     } catch (error) {
-      toast.error("Enter a valid url");
+      toast.error("Not a valid url");
     } finally {
       setLoading(false);
     }
@@ -32,19 +34,43 @@ export default function ShortUrlRedirect() {
     }
   }, [shortUrl]);
 
+  useEffect(() => {
+    if (url) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('HEAD', url);
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          setIframeCompatible(true);
+        } else {
+          setIframeCompatible(false);
+        }
+      };
+      xhr.onerror = () => {
+        setIframeCompatible(false);
+      };
+      xhr.send();
+    }
+  }, [url]);
+
   return (
     <>
-    <nav className='h-[40px] bg-gray-500'>Panel</nav>
-      {loading ? (
-        <div className='flex items-center justify-center'><Spinner/></div>
-      ) : url ? (
-        <div className='flex'>
-        <iframe src={url} title="iframe" className='w-full h-[800px]' allowFullScreen></iframe>
-        <div className='w-[320px] bg-blue-150'></div>
-        </div>
-      ) : (
-        <p>Enter a valid url</p>
-      )}
-    </>
+    <nav className='h-[40px] bg-blue-500'>Panel</nav>
+    {loading ? (
+      <div className='flex items-center justify-center mt-20'><Spinner/></div>
+    ) : (
+      <div className='bg-gray-100'>
+        <div className='md:w-7/12 lg:w-5/12 flex items-center justify-center mx-auto'><AdComponent/></div>
+        {iframeCompatible ? (
+          <iframe src={url} title="iframe" className='w-full h-[800px] '  allowFullScreen></iframe>
+        ) : (
+           <div className='flex flex-col items-center gap-4 justify-center'>
+           <h3 className='text-lg text-slate-500 font-semibold'>Url is not campatible to load in iframe</h3>
+           <h4 className='text-lg text-slate-500 font-semibold'>Go to original url</h4>
+           <a href={url} target='_blank' className='py-2 px-3 bg-blue-150 rounded-md text-white-25'>{url}</a>
+           </div>
+        )}
+      </div>
+    )}
+  </>
   );
 }
